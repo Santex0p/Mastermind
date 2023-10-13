b203be7386f5bf58d91089f43cd3e169130c4e3d
Santiago Escobar Toro <pn63pgk@eduvaud.ch>
2023-10-13 15:27:06 +0200

    Updates for the user

--- Program.cs
+++ Program.cs
@@ -26,6 +26,8 @@ namespace Mastermind
             // Base of the game 
             for (int r = 0; r < 10; r++)
             {
+                Console.WriteLine("*****Game start!******");
+
                 /* Is the character used to initialize the string
                    in the while loop to ensure that no more than 4 letters are generated */
                 string secret = "";
@@ -52,73 +54,105 @@ namespace Mastermind
                     }
 
                 }
+                // Table of position attemps************
+                char[] position = new char[4];
+                for (int i = 0; i < position.Length; i++)
+                {
+                    position[i] = secret[i];
+                }
+
                 // Secret Combination 
                 Console.WriteLine($"secret is {secret}");
 
                 //Attemps of loses and of Games played 
                 int attempts = 17;
-                
+
                 // Instructions 
                 Console.WriteLine("You need to find the secret color combination with the letters:");
                 Console.WriteLine("R-G-B-Y");
+
+                // Attempt variable
                 string guess = Console.ReadLine().ToUpper();
 
                 // Conditions of Win
-                    if (guess == secret)
-                    {
+                if (guess == secret)
+                {
+                    Console.WriteLine("*******You Win :D*******");
                     // Message of win and option of Re-play
-                        choice();
+                    choice();
                     if (Console.ReadLine().ToUpper() == "N")
                     {
                         break;
                     }
-                    // Message of Warning
-                        else
+                }
+                else
+                {
+                    // Conditions of lose 
+                    for (int y = 0; y < attempts; y++)
                     {
-                        Console.WriteLine("Isn't an option. If you do not choose an option the game will close.");
-                        choice();
-                        if (Console.ReadLine().ToUpper() == "Y")
+                        Console.Clear();
+                        Console.WriteLine("-------You lose :(, Try again-------");
+
+                        // Shows the signs
+                        for (int i = 0; i < guess.Length; i++)
                         {
-                            Console.WriteLine("Good Game!");
-                        }
-                        else
-                        {
-                            break;
-                        }
-                    }
-                    }
-                    else
-                    {
-                        // Conditions of lose 
-                        for (int y = 0; y < 10; y++)
-                        {
-                            // Shows the signs
-                            for (int i = 0; i < guess.Length; i++)
+                            if (guess[i] == secret[i])
                             {
-                                if (guess[i] == secret[i])
+                                Console.WriteLine("Color " + guess[i] + " is correct");
+                            }
+                        }
+                        // Number of correct positions
+                        for (int i = 0; i < position.Length; i++)
+                        {
+                            if (guess[i] == position[i])
+                            {
+                                Console.WriteLine("Correct positions:" + position[i]);
+                            }
+                        }
+                            // Attemps
+                            attempts--;
+                            Console.WriteLine("you have " + (attempts - 7) + " attempts left");
+
+                            // Conditions of the second attempt to win
+                            if (Console.ReadLine().ToUpper() == secret)
+                            {
+                                attempts = 0;
+                                Console.WriteLine("*******You Win :D*******");
+                                choice();
+                                if (Console.ReadLine().ToUpper() == "N")
                                 {
-                                    Console.WriteLine("symbol " + guess[i] + " is correct");
+                                    break;
                                 }
                             }
-
-                            // Attemps
-                            Console.WriteLine("You lose :(");
-                            attempts--;
-                            Console.WriteLine("you have " + (attempts - 6) + " attempts left");
-                            Console.ReadLine();
-                        }
-                        // Games to play 
-                        played--;
-                        Console.WriteLine("You have " + (played - 6) + " games to play");
+                        Console.WriteLine("Oh no, You are so bad D:");
                     }
+                    // Games to play 
+                    played--;
+                    Console.WriteLine("You have " + (played - 6) + " games to play, press enter to continue");
+                    Console.ReadLine();
+                    Console.Clear();
 
-
-                    void choice()
+                }
+                void choice()
                 {
                     Console.WriteLine("You want to play again [y/n]?");
                 }
+                /*void seconds()
+                {
+                    var timer = new stopwatch();
+                    timer.Start();
+                    for (int i = 0; i < 1000000000; i++)
+                    {
+                        int x = i * i + 1;
+                    }
+                    timer.Stop();
 
-                }
+                    TimeSpan timeTaken = timer.Elapsed;
+                    string foo = "Time taken: " + timeTaken.ToString(@"m\:ss\.fff");
+                    Console.WriteLine(foo);
+                }*/
+
             }
         }
     }
+}
